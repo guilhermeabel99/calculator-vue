0327ca812f9767d0ffd5d7d5d7177c7c832287e6
guilhermeabel99 <gpeuvis@gmail.com>
2021-09-19 18:09:08 -0300

    finish calculator

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator';
+export default {
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style >
+  .bg {
+    background-color: #3C1687;
+    width: 100vw;
+    height: 100vh;
+    padding: 2rem;
+  }
+  html, body {
+      font-family: 'Sora', sans-serif;
+  }
+</style>
